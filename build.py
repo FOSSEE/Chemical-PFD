54c3ce344d8d4b263e6c6f98dd072293a1a74b55
Blaine <rishi1998@gmail.com>
2020-06-20 14:55:08 +0530

    add build scripts

--- build.py
+++ build.py
@@ -3,11 +3,21 @@ from os.path import dirname
 import subprocess
 
 import fbs.cmdline
+import fbs
 
 @command
 def compileResources():
     subprocess.call("pyrcc5 -o src/main/python/resources/resources.py src/main/ui/resources.rcc", shell=True)
 
+@command
+def symbolGen():
+    exec(open('src/main/python/utils/custom.py').read())
+    
+@command
+def build():
+    compileResources()
+    subprocess.call("fbs freeze", shell=True)
+    
 if __name__ == '__main__':
     project_dir = dirname(__file__)
     fbs.cmdline.main(project_dir)
